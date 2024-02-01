4c96db20e1afc96ee10849dad97dbfd9f81b3744
Cecilia <109989926+Eon4@users.noreply.github.com>
2024-02-01 13:57:49 +0100

    infopage with maps added

--- medie/src/pages/Infopage/Infopage.jsx
+++ medie/src/pages/Infopage/Infopage.jsx
@@ -1,5 +1,35 @@
-// import style from '../styles/mainpage.module.scss'
+import style from './Infopage.module.scss'
+import MapImg from "../../assets/img/mediesuset-map.jpg"
+import MapImg02 from "../../assets/img/googlemap.png"
 
 export const Infopage = () => {
-  return <div>Infopage</div>;
+  return (
+    <div className={style.infopage}>
+      <h1>Her finder du os</h1>
+      <div className={style.gridContainer}>
+        <div className={style.mapContainer}>
+          <img
+            src={MapImg} 
+            alt="Map"
+            className={style.mapImage}
+          />
+          <img
+            src={MapImg02} 
+            alt="Map"
+            className={style.mapImage}
+          />
+        </div>
+        <div className={style.addressContainer}>
+          <h2>Adresse:</h2>
+          <p className={style.address}>
+            Øster Uttrup Vej 1,          
+            <br />
+            9000 Aalborg
+            <br />
+            Denmark
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
